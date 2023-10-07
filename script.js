f6111c4ffadbe1ed87fcc7af5b122288ef4cd8bd
Mhmd Makkeh <coretwo10@gmail.com>
2023-10-07 17:39:33 +0300

    added ui

--- script.js
+++ script.js
@@ -1,47 +1,103 @@
-function ComputerPlay(){
-   let number =Math.floor(Math.random()*3);
-   let ComputerChoice;
-    if(number===0){
-         ComputerChoice='rock';
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+const status = document.getElementById("status");
+
+const computer_score = document.getElementById("computer-score");
+const your_score = document.getElementById("your-score");
+
+const random_array = ["1", "2", "3"];
+
+let which_clicked = "";
+
+let computer_points = 0;
+let your_points = 0;
+
+function randomSequence () {
+  let sequence = random_array[Math.floor(Math.random() * random_array.length)];
+
+  if (sequence === "1") {
+    if (which_clicked === "rock") {
+      status.innerText = "Tie!";
     }
-    if(number===1){
-     ComputerChoice='paper'; 
+
+    else if (which_clicked === "paper") {
+      status.innerText = "You won this round!";
+
+      your_points = your_points + 1;
+      your_score.innerText = "Your Score: " + your_points;
     }
-    if(number===2){
-        ComputerChoice='scissors'
+
+    else if (which_clicked === "scissors") {
+      status.innerText = "The computer won this round!";
+
+      computer_points = computer_points + 1;
+      computer_score.innerText = "Computer Score: " + computer_points;
     }
-    return ComputerChoice.toLowerCase();
+  }
+
+  else if (sequence === "2") {
+    if (which_clicked === "rock") {
+      status.innerText = "The computer won this round!";
+
+      computer_points = computer_points + 1;
+      computer_score.innerText = "Computer Score: " + computer_points;
+    }
+
+    else if (which_clicked === "paper") {
+      status.innerText = "Tie!";
+    }
+
+    else if (which_clicked === "scissors") {
+      status.innerText = "You won this round!";
+
+      your_points = your_points + 1;
+      your_score.innerText = "Your Score: " + your_points;
+    }
+  }
+
+  else if (sequence === "3") {
+    if (which_clicked === "rock") {
+      status.innerText = "You won this round!";
+
+      your_points = your_points + 1;
+      your_score.innerText = "Your Score: " + your_points;
+    }
+
+    else if (which_clicked === "paper") {
+      status.innerText = "The computer won this round!";
+
+      computer_points = computer_points + 1;
+      computer_score.innerText = "Computer Score: " + computer_points;
+    }
+
+    else if (which_clicked === "scissors") {
+      status.innerText = "Tie!";
+    }
+  }
 }
 
-function PlayChoice(){
-   let choice;
-   while(choice!='Rock'|| choice!='Paper'|| choice!='Scissors'){
-    choice=prompt("Choose Rock, Paper, or Scissors", "Choose");
-    choice=choice.toLowerCase();
-    return choice;
-    break;
-   }
-  
-}
-PlayChoice();
-function game(n){
-    let computerScore=0;
-    let playerScore=0;
-   for(let i=0;i<n;i++){
-   let ComputerChoice= PlayChoice();
-    let playerChoice= ComputerPlay();
-   if(ComputerChoice===playerChoice){
-    console.log('Draw');
-    console.log(`${computerScore}:${playerScore}`);
-   }
-   if(ComputerChoice > playerChoice){
-    console.log('Computer Win');
-    console.log(`${computerScore=computerScore + 1}:${playerScore}`);
-   }
-   if(playerChoice > ComputerChoice){
-    console.log('Player win');
-    console.log(`${computerScore}:${playerScore= playerScore + 1}`);
-   }
-}
-}
-game(5);
+$("#rock").click(function () {
+  which_clicked = "rock";
+
+  console.log("Rock selected");
+
+  randomSequence();
+});
+
+$("#paper").click(function () {
+  which_clicked = "paper";
+
+  console.log("Paper selected");
+
+  randomSequence();
+});
+
+$("#scissors").click(function () {
+  which_clicked = "scissors";
+
+  console.log("Scissors selected");
+
+  randomSequence();
+});
